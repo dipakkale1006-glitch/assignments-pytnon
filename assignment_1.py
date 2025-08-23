c97a222a5759efff7e9f011873d22ff1c209da7a
Dipak Dattatray Kale <dipakkale1006@gmail.com>
2025-08-23 18:45:27 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,25 @@
+
+
+#task_1
+a=input('Enter first your  name: ')
+b=input('Enter your second name: ')
+print("hello,", a,b +"! Wellcome to the python program ")
+
+
+
+
+
+
+#task_2
+a= input('Enter the Frist number:')
+b= input('Enter the second number:')
+a=int(a)
+b=int(b)
+c= a+b
+d= a-b
+e= a*b
+f=a/b
+print('addition:',a)
+print('subtraction:',b)
+print('multiplication:',c)
+print('division:',d)
