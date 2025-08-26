13994a82231f159e17ef85982974848ee7edb1e7
Dipak Dattatray Kale <dipakkale1006@gmail.com>
2025-08-26 22:57:49 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,11 @@
+'''a = int(input('Enter number: '))
+if a % 2 == 0:
+  print(a,'is an even')
+else:
+  print(a,'is an odd')'''
+
+total=0
+for i in range(1,51):
+    total=total+i
+print('the sum of number 1 to 50 is:',total)
+
